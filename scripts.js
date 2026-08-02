4110703b7c9ae4cad1ac0d8f3c6410e8d7229490
w <wwong2025+odin@users.noreply.github.com>
2026-08-02 16:55:15 +0800

    UI completed

--- scripts.js
+++ scripts.js
@@ -1,9 +1,72 @@
-console.log("Hello world")
+//const { createElement } = require("react")
+
+//console.log("Welcome to rock, paper, scissors game")
 let humanScore = 0
 let computerScore = 0
+const rps = ["rock","paper","scissors"]
 
 //console.log(getComputerChoice())
 //console.log(getHumanChoice())
+
+const bodyHTML = document.querySelector("body")
+
+const scores = document.createElement("div")
+scores.classList.add ("scores")
+scores.style.display = "flex"
+scores.style.flexDirection = "column"
+scores.style.justifyContent = "center"
+scores.style.alignItems = "center"
+scores.style.padding = "20px"
+scores.style.border = "10px solid green"
+scores.style.borderRadius = "5px"
+scores.style.backgroundColor = "orange"
+scores.style.color = "red"
+
+const titleLine = document.createElement("div")
+titleLine.textContent = "Scores"
+titleLine.style.color = "black"
+titleLine.style.fontSize = "20px"
+titleLine.style.fontWeight = "bold"
+titleLine.style.padding = "10px"
+scores.appendChild(titleLine)
+
+const numbersLine = document.createElement("div")
+numbersLine.textContent = `Human ${humanScore} : ${computerScore} Computer`
+numbersLine.style.fontSize = "14px"
+scores.appendChild(numbersLine)
+
+bodyHTML.appendChild(scores)
+
+
+
+for (let c of rps) {
+    const buts = document.createElement("button")
+    buts.className = c
+    buts.textContent = c
+    buts.addEventListener("click", btn_func)
+    bodyHTML.appendChild(buts)
+}
+
+function btn_func (e) {
+    target = e.target.className
+    //alert (`target : ${target}`)
+    if (rps.includes(target)) {
+        alert(playRound (target, getComputerChoice()))
+        numbersLine.textContent = `Human ${humanScore} : ${computerScore} Computer`
+
+        if (humanScore == 5 || computerScore == 5) {
+            const endGame = document.createElement("div")
+            endGame.textContent = `${humanScore == 5 ? "Human":"Computer"} won !`
+            scores.appendChild(endGame)
+            document.querySelectorAll("button").forEach (btn => {btn.disabled = true})
+        }
+
+    } else {
+        alert ("ERROR: input incorrect")
+    }
+}
+
+
 
 function getComputerChoice(){
     let x = Math.random()
@@ -21,6 +84,7 @@ function getHumanChoice(){
 function playRound (humanChoice, computerChoice) {
     humanChoice = humanChoice.toLowerCase()
     computerChoice = computerChoice.toLowerCase()
+    let message = ""
     if (humanChoice === computerChoice) {
         message = `Human: ${humanChoice}, computer: ${computerChoice}: Draw !`
     } else if ((humanChoice==="rock" && computerChoice === "scissors") ||
@@ -36,13 +100,9 @@ function playRound (humanChoice, computerChoice) {
     return message
 }
 
-//console.log(playRound(getHumanChoice(),getComputerChoice()))
+/*
 
 function playGame(number_of_rounds = 5){
-    for (let i = 0; i < number_of_rounds; i++) {
-        alert(playRound(getHumanChoice(),getComputerChoice()))
-        //alert("hello")
-    }
     if (humanScore > computerScore) {
         message = `You scored ${humanScore} vs computer ${computerScore}. You win the game!`
     }   else if (humanScore === computerScore){
@@ -50,8 +110,9 @@ function playGame(number_of_rounds = 5){
     }   else { 
         message = `You scored ${humanScore} but computer scored ${computerScore}. You lose the game.`
     }
-    console.log (message)
+    //console.log (message)
     alert(message)
 }
 
 playGame()
+*/
